c196d15e171fb0f78a5af75a4f6726c5bbabec17
user61400 <113988019+user61400@users.noreply.github.com>
2022-09-25 21:06:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from discord.ext import commands
+import os
+from os import system
 import jishaku
 
 client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
-    await client.load_extension("jishaku")
-    print("Logged in as " + client.user)
+    client.load_extension("jishaku")
+    system("cls")
+    system("title Jishaku")
+    print("Jishaku succesfully loaded!")
 
-client.run("token")
+client.run("token here")
